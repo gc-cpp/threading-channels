6b5250286caaf9d54ec1426f47d9fb3910e3c918
Sasha Leonov <leonov22.a.v@gmail.com>
2023-09-11 15:21:25 +0700

    [tc-2] Added tests.

--- threading-channels/threading-channels-tests/ActionUserTests.cs
+++ threading-channels/threading-channels-tests/ActionUserTests.cs
@@ -14,6 +14,71 @@ namespace threading_channels_tests;
 
 public class ActionUserTests
 {
+    private readonly WebApplicationFactory<Program> _webApplicationFactory;
+
+    public ActionUserTests()
+    {
+        _webApplicationFactory = GetWebApplicationFactory();
+        var dbContextFactory =
+            _webApplicationFactory.Services.GetRequiredService<IDbContextFactory<UserActionContext>>();
+        using var context = dbContextFactory.CreateDbContext();
+        context.Database.Migrate();
+    }
+    
+    [Fact]
+    public async Task AddUserActions_Ok()
+    {
+        const int userCount = 50;
+        const int userMessageCount = 50;
+        const int userMessageDelay = 100;
+        const int userMessagesDelay = 25000;
+        
+        var fixture = new Fixture();
+        var userIds = fixture.CreateMany<Guid>(userCount);
+
+        foreach (var userId in userIds)
+        {
+            Task.Run(async () =>
+            {
+                var client = _webApplicationFactory.CreateClient();
+                await client.PostAsync($"/channel/subscribe/{userId}", null);
+                for (var i = 0; i < userMessageCount; i++)
+                {
+                    // immitate user delay
+                    await Task.Delay(userMessageDelay);
+                    var payload = new UserAction() { UserId = userId.ToString(), Action = i.ToString() };
+                    var stringPayload = JsonConvert.SerializeObject(payload);
+                    var httpContent = new StringContent(stringPayload, Encoding.UTF8, "application/json");
+                    await client.PostAsync("/channel/action", httpContent);
+                }
+            });
+        }
+
+        await Task.Delay(userMessagesDelay);
+
+        var dbContextFactory =
+            _webApplicationFactory.Services.GetRequiredService<IDbContextFactory<UserActionContext>>();
+        await using var context = await dbContextFactory.CreateDbContextAsync();
+        foreach (var userId in userIds)
+        {
+            Task.Run(async () =>
+            {
+                var client = _webApplicationFactory.CreateClient();
+                await client.PostAsync($"/channel/unsubscribe/{userId}", null);
+            });
+
+            var userActions = await context.UserActions
+                .Where(x => string.Equals(x.UserId, userId.ToString()))
+                .OrderBy(x => x.CreatedOn)
+                .ToListAsync();
+            
+            for (var i = 1; i < userActions.Count; i++)
+            {
+                Assert.False(int.Parse(userActions[i].Action) < int.Parse(userActions[i - 1].Action));
+            }
+        }
+    }
+    
     private WebApplicationFactory<Program> GetWebApplicationFactory()
     {
         return new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
@@ -36,55 +101,5 @@ public class ActionUserTests
 
             });
         });
-    }
-    
-    [Fact]
-    public async Task AddUserActions_Ok()
-    {
-        var fixture = new Fixture();
-        var webApplicationFactory = GetWebApplicationFactory();
-        var dbContextFactory = webApplicationFactory.Services.GetService<IDbContextFactory<UserActionContext>>();
-        await using var context = await dbContextFactory.CreateDbContextAsync();
-        await context.Database.MigrateAsync();
-        var userIds = fixture.CreateMany<Guid>(50);
-
-        foreach (var userId in userIds)
-        {
-            Task.Run(async () =>
-            {
-                var client = webApplicationFactory.CreateClient();
-                await client.PostAsync($"/channel/subscribe/{userId}", null);
-                for (var i = 0; i < 50; i++)
-                {
-                    // immitate user delay
-                    await Task.Delay(100);
-                    var payload = new UserAction() { UserId = userId.ToString(), Action = i.ToString() };
-                    var stringPayload = JsonConvert.SerializeObject(payload);
-                    var httpContent = new StringContent(stringPayload, Encoding.UTF8, "application/json");
-                    await client.PostAsync("/channel/action", httpContent);
-                }
-            });
-        }
-
-        await Task.Delay(25000);
-        
-        foreach (var userId in userIds)
-        {
-            Task.Run(async () =>
-            {
-                var client = webApplicationFactory.CreateClient();
-                await client.PostAsync($"/channel/unsubscribe/{userId}", null);
-            });
-
-            var userActions = await context.UserActions
-                .Where(x => string.Equals(x.UserId, userId.ToString()))
-                .OrderBy(x => x.CreatedOn)
-                .ToListAsync();
-            
-            for (var i = 1; i < userActions.Count; i++)
-            {
-                Assert.False(int.Parse(userActions[i].Action) < int.Parse(userActions[i - 1].Action));
-            }
-        }
     }
 }
--- threading-channels/threading-channels/Program.cs
+++ threading-channels/threading-channels/Program.cs
@@ -3,10 +3,7 @@ using threading_channels.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddPooledDbContextFactory<UserActionContext>(o =>
@@ -16,8 +13,6 @@ builder.Services.AddScoped<UserService>();
 builder.Services.AddSingleton<ChannelPool>();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 
 app.MapControllers();
 
